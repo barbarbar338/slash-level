94728affae9e98992bd393315f5d179daf1564ed
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2020-12-31 15:26:35 +0300

    🔸 Random Status Message

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -6,15 +6,17 @@ import { IEvent } from "my-module";
 const ReadyEvent: IEvent = {
 	name: "ready",
 	execute: async (client: Core) => {
-		await client.user?.setPresence({
-			activity: {
-				name: random(CONFIG.PRESENCE.activity.name),
-				type: random(CONFIG.PRESENCE.activity.type),
-			},
-			status: random(CONFIG.PRESENCE.status),
-			afk: CONFIG.PRESENCE.afk,
-			shardID: CONFIG.PRESENCE.shardID,
-		});
+		setInterval(async () => {
+			await client.user?.setPresence({
+				activity: {
+					name: random(CONFIG.PRESENCE.activity.name),
+					type: random(CONFIG.PRESENCE.activity.type),
+				},
+				status: random(CONFIG.PRESENCE.status),
+				afk: CONFIG.PRESENCE.afk,
+				shardID: CONFIG.PRESENCE.shardID,
+			});
+		}, 15000);
 		pogger.success(`Logged in as ${client.user?.tag}!`);
 	},
 };
